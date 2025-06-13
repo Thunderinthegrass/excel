851520201ca98d35fc170c81e040cdb2556424ce
ThunderInTheGrass <propeller8924@gmail.com>
2025-06-13 22:36:16 +0800

    добавление плагинов

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,7 @@
-// const path = require('path');
-//
-// module.exports = {
-//   context: path.resolve(__dirname, 'src'),
-//   mode: 'development',
-//   entry: './index.js',
-//   output: {
-//     filename: 'bundle.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   }
-// }
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path')
 
 module.exports = {
@@ -17,7 +9,29 @@ module.exports = {
   mode: 'development',
   entry: './index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle[hash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core')
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {from: path.resolve(__dirname, 'src/favicon.ico'),
+         to: path.resolve(__dirname, 'dist')},
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[hash].css',
+    }),
+  ],
 }
