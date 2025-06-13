3b784308192c9355c261ca819f6e84bb7b4ae72d
ThunderInTheGrass <propeller8924@gmail.com>
2025-06-13 19:43:44 +0800

    инициализация вебпак-проекта

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// const path = require('path');
+//
+// module.exports = {
+//   context: path.resolve(__dirname, 'src'),
+//   mode: 'development',
+//   entry: './index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   }
+// }
+
+const path = require('path')
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
